e12933a0a72372fbab838caacb1604070d54adcf
Jared11889 <Jared11889@gmail.com>
2025-06-22 01:59:25 -0400

    Added support for --verbose flag. Refactored argument error handling.

--- main.py
+++ main.py
@@ -15,21 +15,27 @@ def main(args):
     response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 
 if __name__ == "__main__": 
+    #Check for prompt
+    if len(sys.argv[1]) < 1 or sys.argv[1][0] == "-":
+        print("Please provide a prompt.")
+        sys.exit(1)        
+
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("user_prompt")
-    arg_parser.add_argument("--verbose", action="store_true")
+    arg_parser.add_argument("--verbose", "--Verbose", "--VERBOSE", action="store_true")
     args = arg_parser.parse_args()
 
-    #Check for prompt
-    if len(args.user_prompt) < 1:
-        print("Please provide a prompt.")
-        sys.exit(1)
-    else:
-        main(args)
+    main(args)
+
+
+
 else:
     print("Please run from main.py")
     sys.exit(1)
